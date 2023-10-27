60b527824fef033e257aa332bae7811f9a449428
Aviral Verma <aviral.robin@gmail.com>
2023-10-28 02:51:47 +0530

    :bug: Fix darkmode header color placeholder for textform

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 
 export default function TextForm(props) {
-    const [Text, setText] = useState("Enter text here");
+    const [Text, setText] = useState("");
     const handleOnChange = (event) =>{
         setText(event.target.value);
     }
@@ -21,10 +21,12 @@ export default function TextForm(props) {
       props.showAlert("Text cleared","success")
     }
   return <>
-  <div className="container my-2" style={{color: props.mode==='dark'?'white':'black'}}>
+  <div className="container my-2" 
+  // style={{color: props.mode==='dark'?'white':'black'}}
+  >
     <h1>{props.heading}</h1>
     <div className="mb-3">
-    <textarea className="form-control" value={Text} onChange={handleOnChange} id="MyTextArea" rows="8" style={{color: props.mode==='dark'?'white':'black',
+    <textarea className="form-control" value={Text} placeholder='Enter text here' onChange={handleOnChange} id="MyTextArea" rows="8" style={{color: props.mode==='dark'?'white':'black',
     backgroundColor: props.mode==='dark'?'#2c3135':'white'}}></textarea>
     </div>
     <button disabled={Text.length===0} className={`btn btn-${props.btnColor} mx-1 my-1`} onClick={handleUpClick}>Convert to uppercase</button>
@@ -32,7 +34,9 @@ export default function TextForm(props) {
     <button disabled={Text.length===0} className={`btn btn-${props.btnColor} mx-1 my-1`} onClick={handleClearClick}>Clear text</button>
     {/* <button disabled={Text.length===0} className={`btn btn-${props.btnColor} mx-1 my-1`} onClick={handleCopyClick}>Copy text</button>  */}
   </div>
-  <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+  <div className="container my-3" 
+  // style={{color: props.mode==='dark'?'white':'black'}}
+  >
     <h1>Your text summary</h1>
     <p>{Text.split(" ").filter((element)=>{return element.length!==0}).length} words and {Text.length} characters</p>
     <p>Can be read in {0.008*Text.split(" ").length} minutes</p>
